250c88dadc7d5ea5b811d9b166a8d46c0af8fbdc
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-11-21 12:47:57 -0300

    Projeto Envio de Email com Send Grid Inicializado

---
+++ src/test/java/com/example/integratios/IntegratiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.integratios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegratiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
