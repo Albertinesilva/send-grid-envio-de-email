6c100bdc9cab4fbba734b40f8870a9bfc35b5263
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-11-21 15:48:50 -0300

    Pom.xml, EmailResource e EmailService

--- src/main/java/com/example/integrations/resources/EmailResource.java
+++ src/main/java/com/example/integrations/resources/EmailResource.java
@@ -1,5 +1,6 @@
 package com.example.integrations.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.integrations.dto.EmailDTO;
+import com.example.integrations.services.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailResource {
 
+  @Autowired
+  private EmailService emailService;
+
   @PostMapping
   public ResponseEntity<Void> sendEmail(@RequestBody EmailDTO emailDTO) {
+    emailService.sendEmail(emailDTO);
     return ResponseEntity.noContent().build();
   }
 }
