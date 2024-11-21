c81ec656b7438896bb63cc2f29c1db1af3fc6ee1
Albertinesilva <20191tadssaj0026@ifba.edu.br>
2024-11-21 16:15:56 -0300

    Sending email, exception handling

---
+++ src/main/java/com/example/integrations/services/exceptions/EmailException.java
@@ -0,0 +1,9 @@
+package com.example.integrations.services.exceptions;
+
+public class EmailException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public EmailException(String message) {
+    super(message);
+  }
+}
